60a352484cf6c99443d1e6b1ed26f6480f49ad4e
mage-icons <bugthedebugger@gmail.com>
2023-09-17 04:07:35 +0000

    Update from https://github.com/Mage-Icons/mage-icons/commit/85ea0513035cc5102920d4767735eda29d3aeb92

---
+++ social-bw/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "AeroplaneIcon", {
+  enumerable: true,
+  get: function get() {
+    return _AeroplaneIcon.default;
+  }
+});
+var _AeroplaneIcon = _interopRequireDefault(require("./AeroplaneIcon"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
